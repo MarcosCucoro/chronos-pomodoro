73af77343dd088b6e62bb1e9f64ead015a6381ee
Marcos Cucoro <marcos.cucoro@refit.com.br>
2025-05-09 17:32:12 -0300

    Refactor App component structure and styles; add new components for Countdown, Cycles, Logo, Menu, Footer, and DefaultInput; remove unused CSS modules.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,61 @@
-import { Container } from "./components/Container/Container";
-import { Heading } from "./components/Heading/Heading";
 import "./styles/global.css";
 import "./styles/theme.css";
 
+import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
+import { Countdown } from "./components/CountDown";
+import { DefaultInput } from "./components/DefaultInput";
+import { Cycles } from "./components/Cycles";
+import { DefaultButton } from "./components/DefaultButton";
+import { PlayCircleIcon } from "lucide-react";
+import { Footer } from "./components/Footer";
+import { Heading } from "./components/Heading";
+import { useState } from "react";
+
 function App() {
+  const [numero, setNumero] = useState(0);
+
+  const handleClick = () => {
+    setNumero((prev) => prev + 1);
+  };
+
   return (
     <>
+      <Heading>Número: {numero}</Heading>
+      <button onClick={handleClick}>Aumenta</button>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo />
       </Container>
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
+      </Container>
+      <Container>
+        <Countdown />
+      </Container>
+      <Container>
+        <form action="" className="form">
+          <div className="formRow">
+            <DefaultInput
+              labelText="Task"
+              id="meuInput"
+              type="text"
+              placeholder="Digite aqui"
+            />
+          </div>
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="formRow">
+            <Cycles />
+          </div>
+          <div className="formRow">
+            <DefaultButton icon={<PlayCircleIcon />} color="green" />
+          </div>
+        </form>
+      </Container>
+      <Container>
+        <Footer />
       </Container>
     </>
   );
